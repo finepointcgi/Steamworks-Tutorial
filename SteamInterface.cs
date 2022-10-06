dc43ca9db062a0e4cf09f1b894c71afa010849c6
FinepointCGI <Finepointcgi@gmail.com>
2022-10-06 18:25:31 -0400

    Added in player spawn positions

--- SteamInterface.cs
+++ SteamInterface.cs
@@ -9,6 +9,7 @@ public class SteamInterface : Control
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+   
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
